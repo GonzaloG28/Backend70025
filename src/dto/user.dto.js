ced16704e61044c4f6b5bb6b3ff0164d37e7c165
GonzaloG28 <gonzalogimenez280@gmail.com>
2024-10-01 16:18:30 -0300

    implementamos DTO para que no se filtren datos privados del usuario y creamos middleware para que solo el admin realize ciertas acciones

---
+++ src/dto/user.dto.js
@@ -0,0 +1,9 @@
+class userDTO{
+    constructor(user){
+        this.firstName = user.first_name
+        this.lastName = user.last_name
+        this.fullName=`${usuario.nombre} ${usuario.apellido}`
+        this.email = user.email
+        this.role = user.role
+    }
+}
